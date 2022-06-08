97f0cb2e12f433bae29f3cd9426389653f492772
Avtandil Gachechiladze <avtandili.gachechiladze.0@gmail.com>
2022-06-08 11:17:05 +0400

    run prettier in entire project

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  e2e: {
-    viewportHeight: 1920,
-    viewportWidth: 1080,
-    watchForFileChanges: false,
-    baseUrl: 'https://www.saucedemo.com/',
-    video: false,
-    screenshotOnRunFailure: false,
-    trashAssetsBeforeRuns: true,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        viewportHeight: 1920,
+        viewportWidth: 1080,
+        watchForFileChanges: false,
+        baseUrl: 'https://www.saucedemo.com/',
+        video: false,
+        screenshotOnRunFailure: false,
+        trashAssetsBeforeRuns: true,
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
 });
