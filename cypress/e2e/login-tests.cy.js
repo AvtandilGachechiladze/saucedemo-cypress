e72f7876524828ea607993c4de266f4bf67dd074
Avtandil Gachechiladze <avtandili.gachechiladze.0@gmail.com>
2022-06-09 16:48:57 +0400

    modify login tests, add TODO for later updates

--- cypress/e2e/login-tests.cy.js
+++ cypress/e2e/login-tests.cy.js
@@ -8,12 +8,23 @@ beforeEach(() => {
 });
 
 describe('User', () => {
+    /* TODO
+        check for multiple users, including performance glitch and problem users
+        and add more reliable way to confirm that user is logged in and redirected to inventory page
+    */
+    /*
     for (const user in users) {
-        it('should login and be redirected to inventory page', () => {
+        it.skip('should login and be redirected to inventory page', () => {
             cy.submitLoginForm(users[user], passwords.correct);
             cy.url().should('contain', '/inventory.html');
         });
     }
+    */
+
+    it('should login and be redirected to inventory page', () => {
+        cy.submitLoginForm(users.standardUser, passwords.correct);
+        cy.url().should('contain', '/inventory.html');
+    });
 
     it('should be shown locked out error message', () => {
         cy.submitLoginForm(users.lockedOutUser, passwords.correct);
