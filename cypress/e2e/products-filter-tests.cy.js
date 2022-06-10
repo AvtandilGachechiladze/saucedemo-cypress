91911535a3b989ca2968475b7c9c43e7fa6c5a6e
Avtandil Gachechiladze <avtandili.gachechiladze.0@gmail.com>
2022-06-10 16:30:26 +0400

    add getAndVerifyTextElementsSort and getAndVerifyNumericElementsSort commands, update filter tests

--- cypress/e2e/products-filter-tests.cy.js
+++ cypress/e2e/products-filter-tests.cy.js
@@ -11,59 +11,21 @@ beforeEach(() => {
 describe('Products', () => {
     it('should be filtered by name(a to z)', () => {
         cy.get(productsPage.sortButton).select('az');
-
-        cy.get(productsPage.itemNames).then(($els) =>
-            cy.wrap(Cypress._.map($els, 'innerText')).then((val) => {
-                expect(val).to.deep.eq(Array.from(val).sort());
-            })
-        );
+        cy.getAndVerifyTextElementsSort(productsPage.itemNames);
     });
 
     it('should be filtered by name(z to a)', () => {
         cy.get(productsPage.sortButton).select('za');
-
-        cy.get(productsPage.itemNames).then(($els) =>
-            cy.wrap(Cypress._.map($els, 'innerText')).then((val) => {
-                expect(val).to.deep.eq(Array.from(val).sort().reverse());
-            })
-        );
+        cy.getAndVerifyTextElementsSort(productsPage.itemNames, true);
     });
 
     it('should be filtered by price(low to high)', () => {
         cy.get(productsPage.sortButton).select('lohi');
-
-        cy.get(productsPage.itemPrices).then(($els) =>
-            cy.wrap(Cypress._.map($els, 'innerText')).then((val) => {
-                val.forEach((element, index) => {
-                    val[index] = element.substring(1);
-                });
-
-                expect(val).to.deep.eq(
-                    Array.from(val).sort(function (a, b) {
-                        return a - b;
-                    })
-                );
-            })
-        );
+        cy.getAndVerifyNumericElementsSort(productsPage.itemPrices);
     });
 
     it('should be filtered by price(high to low)', () => {
         cy.get(productsPage.sortButton).select('hilo');
-
-        cy.get(productsPage.itemPrices).then(($els) =>
-            cy.wrap(Cypress._.map($els, 'innerText')).then((val) => {
-                val.forEach((element, index) => {
-                    val[index] = element.substring(1);
-                });
-
-                expect(val).to.deep.eq(
-                    Array.from(val)
-                        .sort(function (a, b) {
-                            return a - b;
-                        })
-                        .reverse()
-                );
-            })
-        );
+        cy.getAndVerifyNumericElementsSort(productsPage.itemPrices, true);
     });
 });
