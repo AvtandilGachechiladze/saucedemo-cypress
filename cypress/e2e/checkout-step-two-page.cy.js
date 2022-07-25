7415a1d211410d52a3cb40e0eeca2b1405306d67
Avtandil Gachechiladze <avtandili.gachechiladze.0@gmail.com>
2022-07-25 13:24:20 +0400

    add qase-ids to checkout-step-two-page.cy.js

--- cypress/e2e/checkout-step-two-page.cy.js
+++ cypress/e2e/checkout-step-two-page.cy.js
@@ -1,5 +1,6 @@
 import testData from '../helpers/testData';
 import selectors from '../helpers/selectors';
+import { qase } from 'cypress-qase-reporter/dist/mocha';
 
 const users = testData.account.users;
 const checkoutStepTwo = selectors.checkoutStepTwoPage;
@@ -14,61 +15,21 @@ beforeEach(() => {
 });
 
 describe('Checkout step two page', () => {
-    it('should have payment and shipping information', () => {
-        cy.get(checkoutStepTwo.summeryInfoLabel)
-            .should('have.length', 2)
-            .and('be.visible');
-        cy.get(checkoutStepTwo.summeryValueLabel)
-            .should('have.length', 2)
-            .and('be.visible');
-    });
+    qase(
+        26,
+        it('should have payment and shipping information', () => {
+            cy.get(checkoutStepTwo.summeryInfoLabel)
+                .should('have.length', 2)
+                .and('be.visible');
+            cy.get(checkoutStepTwo.summeryValueLabel)
+                .should('have.length', 2)
+                .and('be.visible');
+        })
+    );
 
-    it('totals should be zero', () => {
-        cy.get(checkoutStepTwo.itemTotal).then((value) => {
-            expect(
-                parseFloat(
-                    value
-                        .text()
-                        .replace(regex.selectEverythingButDigitsCommas, '')
-                )
-            ).to.eq(0);
-        });
-        cy.get(checkoutStepTwo.tax).then((value) => {
-            expect(
-                parseFloat(
-                    value
-                        .text()
-                        .replace(regex.selectEverythingButDigitsCommas, '')
-                )
-            ).to.eq(0);
-        });
-        cy.get(checkoutStepTwo.total).then((value) => {
-            expect(
-                parseFloat(
-                    value
-                        .text()
-                        .replace(regex.selectEverythingButDigitsCommas, '')
-                )
-            ).to.eq(0);
-        });
-    });
-
-    it('should go to finish order page', () => {
-        cy.get(checkoutStepTwo.finishButton).should('be.visible').click();
-        cy.verifyPageIsOpen(links.checkoutComplete, false);
-    });
-
-    it('should go to cart page', () => {
-        cy.get(checkoutStepTwo.cancelButton).should('be.visible').click();
-        cy.verifyPageIsOpen(links.itemsPage, false);
-    });
-
-    context('one item', () => {
-        beforeEach(() => {
-            cy.addItemsToCart('[4]');
-        });
-
-        it('price should be shown correctly', () => {
+    qase(
+        27,
+        it('totals should be zero', () => {
             cy.get(checkoutStepTwo.itemTotal).then((value) => {
                 expect(
                     parseFloat(
@@ -76,11 +37,8 @@ describe('Checkout step two page', () => {
                             .text()
                             .replace(regex.selectEverythingButDigitsCommas, '')
                     )
-                ).to.eq(29.99);
+                ).to.eq(0);
             });
-        });
-
-        it('tax should be shown correctly', () => {
             cy.get(checkoutStepTwo.tax).then((value) => {
                 expect(
                     parseFloat(
@@ -88,11 +46,8 @@ describe('Checkout step two page', () => {
                             .text()
                             .replace(regex.selectEverythingButDigitsCommas, '')
                     )
-                ).to.eq(2.4);
+                ).to.eq(0);
             });
-        });
-
-        it('total should be shown correctly', () => {
             cy.get(checkoutStepTwo.total).then((value) => {
                 expect(
                     parseFloat(
@@ -100,9 +55,85 @@ describe('Checkout step two page', () => {
                             .text()
                             .replace(regex.selectEverythingButDigitsCommas, '')
                     )
-                ).to.eq(32.39);
+                ).to.eq(0);
             });
+        })
+    );
+
+    qase(
+        28,
+        it('should go to finish order page', () => {
+            cy.get(checkoutStepTwo.finishButton).should('be.visible').click();
+            cy.verifyPageIsOpen(links.checkoutComplete, false);
+        })
+    );
+
+    qase(
+        29,
+        it('should go to cart page', () => {
+            cy.get(checkoutStepTwo.cancelButton).should('be.visible').click();
+            cy.verifyPageIsOpen(links.itemsPage, false);
+        })
+    );
+
+    context('one item', () => {
+        beforeEach(() => {
+            cy.addItemsToCart('[4]');
         });
+
+        qase(
+            30,
+            it('price should be shown correctly', () => {
+                cy.get(checkoutStepTwo.itemTotal).then((value) => {
+                    expect(
+                        parseFloat(
+                            value
+                                .text()
+                                .replace(
+                                    regex.selectEverythingButDigitsCommas,
+                                    ''
+                                )
+                        )
+                    ).to.eq(29.99);
+                });
+            })
+        );
+
+        qase(
+            31,
+            it('tax should be shown correctly', () => {
+                cy.get(checkoutStepTwo.tax).then((value) => {
+                    expect(
+                        parseFloat(
+                            value
+                                .text()
+                                .replace(
+                                    regex.selectEverythingButDigitsCommas,
+                                    ''
+                                )
+                        )
+                    ).to.eq(2.4);
+                });
+            })
+        );
+
+        qase(
+            32,
+            it('total should be shown correctly', () => {
+                cy.get(checkoutStepTwo.total).then((value) => {
+                    expect(
+                        parseFloat(
+                            value
+                                .text()
+                                .replace(
+                                    regex.selectEverythingButDigitsCommas,
+                                    ''
+                                )
+                        )
+                    ).to.eq(32.39);
+                });
+            })
+        );
     });
 
     context('several item', () => {
@@ -110,40 +141,58 @@ describe('Checkout step two page', () => {
             cy.addItemsToCart('[4,0]');
         });
 
-        it('price should be shown correctly', () => {
-            cy.get(checkoutStepTwo.itemTotal).then((value) => {
-                expect(
-                    parseFloat(
-                        value
-                            .text()
-                            .replace(regex.selectEverythingButDigitsCommas, '')
-                    )
-                ).to.eq(39.98);
-            });
-        });
+        qase(
+            33,
+            it('price should be shown correctly', () => {
+                cy.get(checkoutStepTwo.itemTotal).then((value) => {
+                    expect(
+                        parseFloat(
+                            value
+                                .text()
+                                .replace(
+                                    regex.selectEverythingButDigitsCommas,
+                                    ''
+                                )
+                        )
+                    ).to.eq(39.98);
+                });
+            })
+        );
 
-        it('tax should be shown correctly', () => {
-            cy.get(checkoutStepTwo.tax).then((value) => {
-                expect(
-                    parseFloat(
-                        value
-                            .text()
-                            .replace(regex.selectEverythingButDigitsCommas, '')
-                    )
-                ).to.eq(3.2);
-            });
-        });
+        qase(
+            34,
+            it('tax should be shown correctly', () => {
+                cy.get(checkoutStepTwo.tax).then((value) => {
+                    expect(
+                        parseFloat(
+                            value
+                                .text()
+                                .replace(
+                                    regex.selectEverythingButDigitsCommas,
+                                    ''
+                                )
+                        )
+                    ).to.eq(3.2);
+                });
+            })
+        );
 
-        it('total should be shown correctly', () => {
-            cy.get(checkoutStepTwo.total).then((value) => {
-                expect(
-                    parseFloat(
-                        value
-                            .text()
-                            .replace(regex.selectEverythingButDigitsCommas, '')
-                    )
-                ).to.eq(43.18);
-            });
-        });
+        qase(
+            35,
+            it('total should be shown correctly', () => {
+                cy.get(checkoutStepTwo.total).then((value) => {
+                    expect(
+                        parseFloat(
+                            value
+                                .text()
+                                .replace(
+                                    regex.selectEverythingButDigitsCommas,
+                                    ''
+                                )
+                        )
+                    ).to.eq(43.18);
+                });
+            })
+        );
     });
 });
